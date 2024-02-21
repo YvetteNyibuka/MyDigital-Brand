d9511cb72f29f15a15d4c2e09fb8e43fa919b140
YvetteNyibuka <izanyibukayvette@gmail.com>
2024-02-21 15:04:24 +0200

    sliders on blogs

---
+++ scripts/blogsSlider.js
@@ -0,0 +1,27 @@
+let currentSlideup = 0;
+
+function showSlide1(n) {
+  const blogElement = document.querySelectorAll(".blog-card");
+  const totalSlides = blogElement.length;
+
+  blogElement.forEach((element) => {
+    element.style.display = "none";
+  });
+
+  currentSlideup = (n + totalSlides) % totalSlides;
+
+  for (let i = 0; i < 4; i++) {
+    const index = (currentSlideup + i) % totalSlides;
+    blogElement[index].style.display = "flex";
+  }
+}
+
+function nextSlide1() {
+  showSlide1(currentSlideup + 4);
+}
+
+function prevSlide1() {
+  showSlide1(currentSlideup - 4);
+}
+
+showSlide1(currentSlideup);
--- scripts/singleblog.js
+++ scripts/singleblog.js
@@ -1,32 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const replyButtons = document.querySelectorAll(".reply-btn");
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get all reply buttons
-    const replyButtons = document.querySelectorAll('.reply-btn');
-
-    // Add click event listeners to each reply button
-    replyButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-        // Get the parent comment container
-        const commentContainer = this.closest('.createdcomment');
-
-        // Toggle the visibility of the reply section
-        const replySection = commentContainer.querySelector('.reply-section');
-        replySection.classList.toggle('show-reply-section');
-      });
-    });
-
-    // Get all cancel reply buttons
-    const cancelReplyButtons = document.querySelectorAll('.cancel-reply');
-
-    // Add click event listeners to each cancel reply button
-    cancelReplyButtons.forEach(function (cancelButton) {
-      cancelButton.addEventListener('click', function () {
-        // Get the parent comment container
-        const commentContainer = this.closest('.createdcomment');
-
-        // Hide the reply section
-        const replySection = commentContainer.querySelector('.reply-section');
-        replySection.classList.remove('show-reply-section');
-      });
+  replyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const commentContainer = this.closest(".createdcomment");
+      const replySection = commentContainer.querySelector(".reply-section");
+      replySection.classList.toggle("show-reply-section");
     });
   });
+
+  const cancelReplyButtons = document.querySelectorAll(".cancel-reply");
+
+  cancelReplyButtons.forEach(function (cancelButton) {
+    cancelButton.addEventListener("click", function () {
+      const commentContainer = this.closest(".createdcomment");
+
+      const replySection = commentContainer.querySelector(".reply-section");
+      replySection.classList.remove("show-reply-section");
+    });
+  });
+});
