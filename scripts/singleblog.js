4c4ede3495d36ecc630c9663b6b95fef4042fbe2
YvetteNyibuka <izanybukayvette@gmail.com>
2024-04-03 12:03:21 +0200

    formatted single blog page

--- scripts/singleblog.js
+++ scripts/singleblog.js
@@ -1,29 +1,30 @@
 document.addEventListener("DOMContentLoaded", async function () {
   let singlblogData = document.getElementById("blogdetails1");
-  const blogInfo = JSON.parse(localStorage.getItem("publishedBlogs")) || [];
+  const loader = document.querySelector(".loaderOverlay");
+  const additions = document.querySelector(".additions")
+
+  function showLoader() {
+    loader.style.display = "flex";
+  }
+
+  function hideLoader() {
+    loader.style.display = "none";
+  }
 
   await readIdFromUrl();
-var currentblog;
+   var currentblog;
 
   async function readIdFromUrl() {
     const urlParams = new URLSearchParams(window.location.search);
     const blogId = urlParams.get("id");
 
-  // Fetch blogs from server
-  async function fetchBlog() {
-    const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-    if (!loggedUser || !loggedUser.token) {
-      console.error("Invalid or missing token");
-      return;
-    }
-    const token = loggedUser.token;
+  // Fetch blog from server
 
+  async function fetchBlog() {
     try {
+      showLoader() 
       const response = await fetch(`https://mybrand-be-rs6b.onrender.com/api/v1/blogs/${blogId}`, {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-        }
+        method: "GET"
       });
 
       if (!response.ok) {
@@ -32,15 +33,17 @@ var currentblog;
 
       const data = await response.json();
       currentblog = data.data;
+      hideLoader() 
     } catch (error) {
       console.error("Error fetching blogs:", error);
+      hideLoader();
     }
   }
   await fetchBlog()
-console.log("================================================", currentblog);
   
-    if (currentblog.singleBlog) {
+    if (currentblog?.singleBlog) {
       singlblogData.innerHTML = `
+  <a href="../index.html" style="color: black;"><h1 style="margin: 2rem;"><i class="fa-solid fa-chevron-left"></i>Back</h1></a>
             <p style="text-align: start">${currentblog.singleBlog?.category}</p>
       <h1 id="blogtitlee">
         ${currentblog.singleBlog?.title}
@@ -65,7 +68,6 @@ console.log("================================================", currentblog);
           ${currentblog.singleBlog?.description}
           </p>
       </div>
-
 
       `;
     }
@@ -102,21 +104,18 @@ console.log("================================================", currentblog);
   
   await fetchBlogs();
 
-  // console.log("+++++++++++++++++++++++++++++++++++++++++++++++", allBlogs);
-  // Display other blogs
+
   const urlParams = new URLSearchParams(window.location.search);
   const blogId = urlParams.get("id");
 
   const nondisplayedBlogs = allBlogs.map(
     (blog1) => blog1.filter(blog => blog._id !== blogId)
   );
-  // console.log("////////////////////////////////////////", nondisplayedBlogs);
   if (nondisplayedBlogs.length > 0) {
     const otherBlogs = document.getElementById("otheravailableblogs");
 
     nondisplayedBlogs.map(blog => {
       blog.map(blog => {
-      console.log("dfshdgygntrfbewsagsxhctvhynjjuk", blog);
       otherBlogs.innerHTML += `
           <div class="available1">
             <div class="picture1">
@@ -140,7 +139,6 @@ console.log("================================================", currentblog);
   const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
   const token = loggedUser.token;
 
-  console.log("single blogId", currentblog);
   commentBtn.addEventListener("click", async () => {
   const Message = document.getElementById("message").value;
   
@@ -163,9 +161,24 @@ try{
           },
           body: JSON.stringify(newComment),
       });
+      const blogComments = await commentResponse.json();
       if(commentResponse.ok){
-        console.log("comentscreated successfully", commentResponse.data);
-        window.location.reload();
+        console.log("comentscreated successfully", blogComments);
+        Toastify({
+          text: `${blogComments?.message}`,
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top", 
+          position: "left", 
+          stopOnFocus: true, 
+          backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+          onClick: function(){} 
+      }).showToast();
+        setTimeout(() => {
+               window.location.reload();
+        }, 3000); 
       }
 
 }catch(e){
@@ -174,20 +187,71 @@ console.log(e);
 
   });
  
-  console.log("comments", currentblog);
 let allComments = currentblog?.singleBlogComments;
+const commentsNumber = document.getElementById("commentsNumber");
+commentsNumber.innerHTML = `${allComments.length} Comments`;
    for(let i = 0; i<allComments?.length; i++){
-        const commentsNumber = document.getElementById("commentsNumber");
         const createdComment = document.getElementById("dynamicContent");
         createdComment.innerHTML += `
         <div class="commentcontent">
         <p> <i class="fa-solid fa-user"></i> ${allComments[i].username} </p>
-        <p id="commentdesc">
-       ${allComments[i].commentMessage}
-        </p>
+        <p style=" word-break: break-all;" id="commentdesc">
+   ${allComments[i].commentMessage}
+      </p>
       </div>`
       
    }
+
+   const loggedUser1 = JSON.parse(localStorage.getItem("loggedUser"));
+   const token1 = loggedUser.token;
+   let  userId = loggedUser?.user?.id;
+
+   async function addLike(userid, blogid) {
+    if (!loggedUser1 || !loggedUser1.token) {
+       console.error("Invalid or missing token");
+       window.location.href = "../pages/login.html";
+    }
+    const newLike = {
+       blogId: blogid,
+       userId: userid,
+       isLiked: true
+    };
+    try {
+       const likeResponse = await fetch(`https://mybrand-be-rs6b.onrender.com/api/v1/blogs/${blogId}/likes`, {
+         method: 'POST',
+         headers: {
+           "Authorization": `Bearer ${token1}`,
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(newLike)
+       });
+       
+       if (!likeResponse.ok) {
+         throw new Error("Failed to add like to blogs");
+       }
+       console.log("Like added successfully");
+       window.location.reload();
+    } catch (e) {
+       console.error("Error adding like:", e);
+    }
+   }
+   additions.innerHTML =` <p>
+   <button style="background-color: white; border: none" class="like-button" data-blog-id="${currentblog?.singleBlog?._id}" data-user-id="${userId}">
+   ${currentblog?.singleBloglikes?.some(like => like.userId === userId) ? 
+     `<img src="https://www.freeiconspng.com/uploads/valentine-red-heart-png-15.png" style="height: 15px; width: 15px;" alt="" />` :
+     `<img src="https://img.freepik.com/premium-vector/like-heart-symbol-icon_165079-3631.jpg" style="height: 15px; width: 15px;" alt="" />`
+   }
+   </button> 
+    ${currentblog?.singleBloglikes?.length}</p>`
+
+    additions.addEventListener('click', function(event) {
+      const likeButton = event.target.closest('.like-button');
+      if (likeButton) {
+         const blogId = likeButton.getAttribute('data-blog-id');
+         const userId = likeButton.getAttribute('data-user-id');
+         addLike(userId, blogId);
+      }
+     });
 
   const replyButtons = document.querySelectorAll(".reply-btn");
   replyButtons.forEach(function (button) {
@@ -197,17 +261,5 @@ let allComments = currentblog?.singleBlogComments;
       replySection.classList.toggle("show-reply-section");
     });
   });
-
-  const cancelReplyButtons = document.querySelectorAll(".cancel-reply");
-
-  cancelReplyButtons.forEach(function (cancelButton) {
-    cancelButton.addEventListener("click", function () {
-      const commentContainer = this.closest(".createdcomment");
-
-      const replySection = commentContainer.querySelector(".reply-section");
-      replySection.classList.remove("show-reply-section");
-    });
-  });
-
  
 });
--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,238 +1,240 @@
-document.addEventListener("DOMContentLoaded", async function () {
+// window.addEventListener("load", async function () {
+//   const form = document.querySelector("#contactForm");
+//   const fullNameInput = document.getElementById("full-names");
+//   const emailInput = document.getElementById("email");
+//   const subjectInput = document.getElementById("subject");
+//   const messageInput = document.getElementById("message");
+//   const submitBtn = document.getElementById("contactbtn");
+//   const loader = document.querySelector(".loaderOverlay");
 
-  const form = document.querySelector("#contactForm");
-  const fullNameInput = document.getElementById("full-names");
-  const emailInput = document.getElementById("email");
-  const subjectInput = document.getElementById("subject");
-  const messageInput = document.getElementById("message");
-  const submitBtn = document.getElementById("contactbtn");
-  const loader = document.querySelector(".loaderOverlay");
+//   function showLoader() {
+//     loader.style.display = "flex";
+//   }
 
+//   function hideLoader() {
+//     loader.style.display = "none";
+//   }
+//   form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     validateForm();
+//   });
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    validateForm();
-  });
+//   fullNameInput.addEventListener("input", validateForm);
+//   emailInput.addEventListener("input", validateForm);
+//   subjectInput.addEventListener("input", validateForm);
+//   messageInput.addEventListener("input", validateForm);
 
-  fullNameInput.addEventListener("input", validateForm);
-  emailInput.addEventListener("input", validateForm);
-  subjectInput.addEventListener("input", validateForm);
-  messageInput.addEventListener("input", validateForm);
+//   function validateForm() {
+//     let isValid = true;
 
-  function validateForm() {
-    let isValid = true;
+//     if (fullNameInput.value.trim() === "") {
+//       isValid = false;
+//       showError(fullNameInput, "Please enter your full name");
+//     } else {
+//       hideError(fullNameInput);
+//     }
 
-    if (fullNameInput.value.trim() === "") {
-      isValid = false;
-      showError(fullNameInput, "Please enter your full name");
-    } else {
-      hideError(fullNameInput);
-    }
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!emailRegex.test(emailInput.value)) {
+//       isValid = false;
+//       showError(emailInput, "Invalid email address");
+//     } else {
+//       hideError(emailInput);
+//     }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(emailInput.value)) {
-      isValid = false;
-      showError(emailInput, "Invalid email address");
-    } else {
-      hideError(emailInput);
-    }
+//     if (subjectInput.value.trim() === "") {
+//       isValid = false;
+//       showError(subjectInput, "Please enter a subject");
+//     } else {
+//       hideError(subjectInput);
+//     }
 
-    if (subjectInput.value.trim() === "") {
-      isValid = false;
-      showError(subjectInput, "Please enter a subject");
-    } else {
-      hideError(subjectInput);
-    }
+//     if (messageInput.value.trim() === "") {
+//       isValid = false;
+//       showError(messageInput, "Please enter your message");
+//     } else {
+//       hideError(messageInput);
+//     }
 
-    if (messageInput.value.trim() === "") {
-      isValid = false;
-      showError(messageInput, "Please enter your message");
-    } else {
-      hideError(messageInput);
-    }
+//     submitBtn.disabled = !isValid;
+//   }
 
-    submitBtn.disabled = !isValid;
-  }
+//   function showError(inputElement, errorMessage) {
+//     const errorElement = document.createElement("small");
+//     errorElement.textContent = errorMessage;
+//     errorElement.style.color = "red";
+//     errorElement.classList.add("error-message");
+//     errorElement.style.fontSize = "10px";
+//     errorElement.style.textAlign = "left";
 
-  function showError(inputElement, errorMessage) {
-    const errorElement = document.createElement("small");
-    errorElement.textContent = errorMessage;
-    errorElement.style.color = "red";
-    errorElement.classList.add("error-message");
-    errorElement.style.fontSize = "10px";
-    errorElement.style.textAlign = "left";
+//     const inputGroup = inputElement.closest(".inputgroup");
+//     const existingError = inputGroup.querySelector(".error-message");
+//     if (existingError) {
+//       inputGroup.removeChild(existingError);
+//     }
+//     inputGroup.appendChild(errorElement);
+//   }
 
-    const inputGroup = inputElement.closest(".inputgroup");
-    const existingError = inputGroup.querySelector(".error-message");
-    if (existingError) {
-      inputGroup.removeChild(existingError);
-    }
-    inputGroup.appendChild(errorElement);
-  }
+//   function hideError(inputElement) {
+//     const inputGroup = inputElement.closest(".inputgroup");
+//     const errorElement = inputGroup.querySelector(".error-message");
 
-  function hideError(inputElement) {
-    const inputGroup = inputElement.closest(".inputgroup");
-    const errorElement = inputGroup.querySelector(".error-message");
-
-    if (errorElement) {
-      inputGroup.removeChild(errorElement);
-    }
-  }
-
-  function showLoader() {
-    loader.style.display = "flex";
-  }
-
-  function hideLoader() {
-    loader.style.display = "none";
-  }
-  // Fetch blogs from server
-
-  async function fetchBlogs() {
-    try {
-    showLoader()
-      const response = await fetch("https://mybrand-be-rs6b.onrender.com/api/v1/blogs", {
-        method: "GET"
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch blogs");
-      }
-
-      const data = await response.json();
-      // console.log("Got blog data", data);
-      renderBlogs(data);
-      hideLoader();
-    } catch (error) {
-      console.error("Error fetching blogs:", error);
-      hideLoader();
-    }
-  }
+//     if (errorElement) {
+//       inputGroup.removeChild(errorElement);
+//     }
+//   }
 
 
-  await fetchBlogs()
+//   // Fetch blogs from server
 
-  function formatDate(dateString) {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
+//   async function fetchBlogs() {
+//     try {
+//      showLoader();
+//       const response = await fetch("https://mybrand-be-rs6b.onrender.com/api/v1/blogs", {
+//         method: "GET"
+//       });
 
-  // Render blogs
-function renderBlogs(blogs) {
-  const blogCardContainer = document.getElementById("blogs-wrapper");
-  blogCardContainer.innerHTML = ""; 
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  const userId = loggedUser?.user?.id;
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch blogs");
+//       }
 
-   blogs?.data?.forEach(blog => {
-    blogCardContainer.innerHTML += `
-      <div class="blog-card">
-        <div class="blog-img">
-          <img src="${blog?.coverImage}" alt="">
-        </div>
-        <div class="blog-description">
-          <p>${blog?.description?.slice(0, 100)}... <a href="./pages/singleblog.html?id=${blog?._id}">Learn More</a></p>
-        </div>
-        <div class="blog-info">
-          <p>${blog?.author ? blog?.author : 'Yvette'}</p>
-          <p>on ${(formatDate(blog?.createdAt))}</p>
-        </div>
-        <div class="blog-statistics">
-          <p>
-          <button style="background-color: white; border: none;" class="like-button" data-blog-id="${blog?._id}" data-user-id="${userId}">
-          ${blog?.likes?.some(like => like.userId === userId) ? 
-            `<img src="https://i.pinimg.com/736x/15/93/f0/1593f08d866e5cf9356f143ecbf3accb.jpg" style="height: 20px; width: 20px;" alt="" />` :
-            `<img src="https://img.freepik.com/premium-vector/like-heart-symbol-icon_165079-3631.jpg" style="height: 20px; width: 20px;" alt="" />`
-          }
-          </button>
-           ${blog?.likes?.length}</p>
-          <p>💬 ${blog?.comments?.length}</p>
-        </div>
-      </div>
-    `;
-  });
+//       const data = await response.json();
+//       // console.log("Got blog data", data);
+//       renderBlogs(data);
+//       hideLoader();
+//     } catch (error) {
+//       console.error("Error fetching blogs:", error);
+//       hideLoader();
+//     }
+//     finally {
+//       hideLoader(); 
+//     }
+//   }
+
+
+//   await fetchBlogs()
+
+//   function formatDate(dateString) {
+//     const date = new Date(dateString);
+//     const year = date.getFullYear();
+//     const month = String(date.getMonth() + 1).padStart(2, '0');
+//     const day = String(date.getDate()).padStart(2, '0');
+//     return `${year}-${month}-${day}`;
+//   }
+
+//   // Render blogs
+// async function renderBlogs(blogs) {
+//   const blogCardContainer = document.getElementById("blogs-wrapper");
+//   blogCardContainer.innerHTML = ""; 
+//   const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+//   const userId = loggedUser?.user?.id;
+
+//    blogs?.data?.forEach(blog => {
+//     blogCardContainer.innerHTML += `
+//       <div class="blog-card">
+//         <div class="blog-img">
+//           <img src="${blog?.coverImage}" alt="">
+//         </div>
+//         <div class="blog-description">
+//           <p id="blog-desc">${blog?.description?.slice(0, 100)}... <a href="./pages/singleblog.html?id=${blog?._id}">Learn More</a></p>
+//         </div>
+//         <div class="blog-info">
+//           <p>${blog?.author ? blog?.author : 'Yvette'}</p>
+//           <p>on ${(formatDate(blog?.createdAt))}</p>
+//         </div>
+//         <div class="blog-statistics">
+//           <p>
+//           <button style="background-color: white; border: none;" class="like-button" data-blog-id="${blog?._id}" data-user-id="${userId}">
+//           ${blog?.likes?.some(like => like.userId === userId) ? 
+//             `<img src="https://i.pinimg.com/736x/15/93/f0/1593f08d866e5cf9356f143ecbf3accb.jpg" style="height: 20px; width: 20px;" alt="" />` :
+//             `<img src="https://img.freepik.com/premium-vector/like-heart-symbol-icon_165079-3631.jpg" style="height: 20px; width: 20px;" alt="" />`
+//           }
+//           </button>
+//            ${blog?.likes?.length}</p>
+//           <p>💬 ${blog?.comments?.length}</p>
+//         </div>
+//       </div>
+//     `;
+//   });
   
   
-blogCardContainer.addEventListener('click', function(event) {
-  const likeButton = event.target.closest('.like-button');
-  if (likeButton) {
-     const blogId = likeButton.getAttribute('data-blog-id');
-     const userId = likeButton.getAttribute('data-user-id');
-     addLike(blogId, userId);
-  }
- });
-}
+// blogCardContainer.addEventListener('click', function(event) {
+//   const likeButton = event.target.closest('.like-button');
+//   if (likeButton) {
+//      const blogId = likeButton.getAttribute('data-blog-id');
+//      const userId = likeButton.getAttribute('data-user-id');
+//      addLike(blogId, userId);
+//   }
+//  });
+// }
 
-async function addLike(blogId, userId) {
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  if (!loggedUser || !loggedUser.token) {
-     console.error("Invalid or missing token");
-     return;
-  }
-  const token = loggedUser.token;
-  const newLike = {
-     blogId: blogId,
-     userId: userId,
-     isLiked: true
-  };
-  try {
-     const likeResponse = await fetch(`https://mybrand-be-rs6b.onrender.com/api/v1/blogs/${blogId}/likes`, {
-       method: 'POST',
-       headers: {
-         "Authorization": `Bearer ${token}`,
-         'Content-Type': 'application/json'
-       },
-       body: JSON.stringify(newLike)
-     });
+// async function addLike(blogId, userId) {
+//   const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+//   if (!loggedUser || !loggedUser.token) {
+//      console.error("Invalid or missing token");
+//      return;
+//   }
+//   const token = loggedUser.token;
+//   const newLike = {
+//      blogId: blogId,
+//      userId: userId,
+//      isLiked: true
+//   };
+//   try {
+//      const likeResponse = await fetch(`https://mybrand-be-rs6b.onrender.com/api/v1/blogs/${blogId}/likes`, {
+//        method: 'POST',
+//        headers: {
+//          "Authorization": `Bearer ${token}`,
+//          'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(newLike)
+//      });
      
-     if (!likeResponse.ok) {
-       throw new Error("Failed to add like to blogs");
-     }
+//      if (!likeResponse.ok) {
+//        throw new Error("Failed to add like to blogs");
+//      }
  
-     console.log("Like added successfully");
-     // Refresh the page or do something else upon successful like addition
-     window.location.reload();
+//      console.log("Like added successfully");
+//      // Refresh the page or do something else upon successful like addition
+//      window.location.reload();
     
-  } catch (e) {
-     console.error("Error adding like:", e);
-  }
- }
+//   } catch (e) {
+//      console.error("Error adding like:", e);
+//   }
+//  }
  
-  // Blog slider
-  let currentSlide = 0;
+//   // Blog slider
+//   let currentSlide = 0;
 
-  function showSlide(n) {
-    const blogCards = document.querySelectorAll(".blog-card");
-    const totalSlides = blogCards.length;
+//   function showSlide(n) {
+//     const blogCards = document.querySelectorAll(".blog-card");
+//     const totalSlides = blogCards.length;
 
-    blogCards.forEach(card => {
-      card.style.display = "none";
-    });
+//     blogCards.forEach(card => {
+//       card.style.display = "none";
+//     });
 
-    currentSlide = (n + totalSlides) % totalSlides;
+//     currentSlide = (n + totalSlides) % totalSlides;
 
-    for (let i = 0; i < 4; i++) {
-      const index = (currentSlide + i) % totalSlides;
-      blogCards[index].style.display = "flex";
-    }
-  }
+//     for (let i = 0; i < 4; i++) {
+//       const index = (currentSlide + i) % totalSlides;
+//       blogCards[index].style.display = "flex";
+//     }
+//   }
 
-  function nextSlide() {
-    showSlide(currentSlide + 1);
-  }
+//   function nextSlide() {
+//     showSlide(currentSlide + 1);
+//   }
 
-  function prevSlide() {
-    showSlide(currentSlide - 1);
-  }
+//   function prevSlide() {
+//     showSlide(currentSlide - 1);
+//   }
 
-  showSlide(currentSlide);
+//   showSlide(currentSlide);
 
-  const nextBtn = document.querySelector(".nextBtn");
-  const prevBtn = document.querySelector(".prevBtn");
-  nextBtn.addEventListener("click", nextSlide);
-  prevBtn.addEventListener("click", prevSlide);
+//   const nextBtn = document.querySelector(".nextBtn");
+//   const prevBtn = document.querySelector(".prevBtn");
+//   nextBtn.addEventListener("click", nextSlide);
+//   prevBtn.addEventListener("click", prevSlide);
 
-});
+// });
